9c41ca288de0217328be86386a244fded765e504
Ajith <ajithraina0606@gmail.com>
2024-09-29 21:38:30 +0530

    Add files via upload

---
+++ control_statements.js
@@ -0,0 +1,38 @@
+// IF condition
+const number = 10;
+ 
+if (number > 0) {
+    console.log("The number is greater than 0");
+};
+
+// If-Else condition
+let example = -10;
+ 
+if (example > 0)
+    console.log("The number is positive.");
+else
+    console.log("The number is negative");
+
+// Switch condition
+
+let switchex = 1;
+ 
+switch (switchex) {
+    case 0:
+        console.log("Number is zero.");
+        break;
+    case 1:
+        console.log("Number is one.");
+        break;
+    case 2:
+        console.log("Number is two.");
+        break;
+     case 3:
+        console.log("Number is three.");
+        break;
+    default:
+        console.log("Number is greater than 3.");
+};
+
+
+
