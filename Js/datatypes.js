cdc9c3b1203fbfbb57d9f4e434ddd004102694df
Ajith <ajithraina0606@gmail.com>
2024-10-07 09:08:57 +0530

    Add files via upload

---
+++ Js/datatypes.js
@@ -0,0 +1,27 @@
+// Numbers
+let price = 1995;
+console.log(typeof price); 
+
+// Strings
+let name = 'Ajith';
+console.log(typeof myname); 
+
+// Boolean
+let isAdmin = true;
+console.log(typeof isAdmin); 
+
+// Null
+let nullValue = null;
+console.log(typeof nullValue); 
+
+// Undefined
+let undef;
+console.log(typeof undef); 
+
+// Object
+let user = { name: 'vijay', age: 25 };
+console.log(typeof user); 
+
+// Array
+let animals = ['lion', 'tiger', 'leopard'];
+console.log(typeof animals); 
